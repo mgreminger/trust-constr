537cbc8e10d65ad4709f54369b2cd9e69bd97c67
mgreminger <michael.greminger@gmail.com>
2021-01-21 17:02:38 -0600

    Initial version of setup.py and README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+## trust-constr
+
+trust-constr optimization algorithm from the SciPy project. This is a version of the trust-constr algorithm that does not depend on the rest of SciPy. The only dependency is NumPy. The goal is to have a version of the trust-constr algorithm that can run within the Pyodide environment. This is a work in progress.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="trust-constr",
+    version="0.0.1",
+    description="trust-constr optimization algorithm from the SciPy project.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mgreminger/trust-constr",
+    packages=['trust_constr'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    requires=['numpy>=1.16.5']
+)
