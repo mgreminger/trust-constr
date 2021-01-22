034ae1d8b9e10ff09161935133f59d7d5d423513
mgreminger <michael.greminger@gmail.com>
2021-01-22 16:21:41 -0600

    Bringing in first set of test from SciPy optimize

--- trust_constr/__init__.py
+++ trust_constr/__init__.py
@@ -1,3 +1,4 @@
 from ._minimize import minimize
 from ._constraints import NonlinearConstraint, LinearConstraint, Bounds
 from .optimize import check_grad
+from ._hessian_update_strategy import BFGS, SR1
